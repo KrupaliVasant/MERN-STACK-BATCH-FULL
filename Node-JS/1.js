6d48f96d976b25fdb9f21bb7c61764108aac5837
KrupaliVasant <70903205+KrupaliVasant@users.noreply.github.com>
2021-08-30 15:10:27 +0530

    formatted

--- Node-JS/1.js
+++ Node-JS/1.js
@@ -1,13 +1,15 @@
 // Create own server in Node Js
 
-const http = require('http');  //add http package to js file
-http.createServer(function(req,res){
+const http = require("http"); //add http package to js file
+http
+  .createServer(function (req, res) {
     // send HTTP header
     // HTTP status:200:OK
     // Content type:text/plain
-    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.writeHead(200, { "Content-Type": "text/plain" });
     // send response body as "hello world"
     res.end("Hello World");
-}).listen(8000);
+  })
+  .listen(8000);
 // console will print the message
-console.log('Server running at http://127.0.0.1:8000');
+console.log("Server running at http://127.0.0.1:8000");
